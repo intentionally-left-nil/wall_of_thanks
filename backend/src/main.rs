9744674236c00a3938cd7bc4fc65d4229aa0d902
Anil Kulkarni <anil@terminal.space>
2024-04-14 17:38:53 -0700

    Implement create route

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -16,6 +16,12 @@ struct Comment {
     approved: bool,
 }
 
+#[derive(serde::Deserialize)]
+struct NewComment {
+    message: String,
+    author: String,
+}
+
 type RouteResponse = Response<Cursor<Vec<u8>>>;
 
 fn row_to_comment(statement: &mut sqlite::Statement) -> Result<Comment, Box<dyn Error>> {
@@ -46,6 +52,21 @@ fn default_response(code: u16) -> RouteResponse {
     );
 }
 
+fn respond_with_json<T>(code: u16, data: &T) -> Result<RouteResponse, Box<dyn Error>>
+where
+    T: ?Sized + serde::Serialize,
+{
+    let data = serde_json::to_string(&data)?.into_bytes();
+    let len = data.len();
+    return Ok(Response::new(
+        StatusCode(code),
+        vec!["Content-Type: application/json".parse::<Header>().unwrap()],
+        Cursor::new(data),
+        Some(len),
+        None,
+    ));
+}
+
 fn list_comments(conn: &sqlite::ConnectionThreadSafe) -> Result<RouteResponse, Box<dyn Error>> {
     let mut statement = conn.prepare("SELECT * FROM comments")?;
     let mut rows: Vec<Result<Comment, Box<dyn Error>>> = Vec::new();
@@ -53,16 +74,37 @@ fn list_comments(conn: &sqlite::ConnectionThreadSafe) -> Result<RouteResponse, B
         rows.push(row_to_comment(&mut statement));
     }
     let comments: Result<Vec<Comment>, Box<dyn Error>> = rows.into_iter().collect();
-    let comments = comments?;
-    let data = serde_json::to_string(&comments)?.into_bytes();
-    let len = data.len();
-    return Ok(Response::new(
-        StatusCode(200),
-        vec!["Content-Type: application/json".parse::<Header>().unwrap()],
-        Cursor::new(data),
-        Some(len),
-        None,
-    ));
+    return respond_with_json(200, &comments?);
+}
+
+fn create_comment(
+    request: &mut Request,
+    conn: &sqlite::ConnectionThreadSafe,
+) -> Result<RouteResponse, Box<dyn Error>> {
+    if !request
+        .headers()
+        .iter()
+        .any(|h| h.field.equiv("Content-Type") && h.value == "application/json")
+    {
+        return respond_with_json(400, "Content-type must be application/json");
+    }
+    let maybe_comment: Result<NewComment, _> = serde_json::from_reader(request.as_reader());
+
+    if let Err(e) = maybe_comment {
+        let data = e.to_string();
+        return respond_with_json(400, &data);
+    }
+    let comment = maybe_comment?;
+    let mut statement =
+        conn.prepare("INSERT INTO comments (message, author) VALUES (?, ?) RETURNING *")?;
+    statement.bind::<&[(_, sqlite::Value)]>(
+        &[(1, comment.message.into()), (2, comment.author.into())][..],
+    )?;
+    if let sqlite::State::Done = statement.next()? {
+        return Err("No row returned".into());
+    }
+    let inserted = row_to_comment(&mut statement)?;
+    return respond_with_json(201, &inserted);
 }
 
 fn initialize_database(path: &Path) -> Result<sqlite::ConnectionThreadSafe, Box<dyn Error>> {
@@ -72,7 +114,7 @@ fn initialize_database(path: &Path) -> Result<sqlite::ConnectionThreadSafe, Box<
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         message TEXT NOT NULL,
         author TEXT,
-        apaproved BOOLEAN NOT NULL DEFAULT 0,
+        approved BOOLEAN NOT NULL DEFAULT 0,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)",
     )?;
     return Ok(conn);
@@ -83,11 +125,12 @@ fn start_server(port: u16, db_conn: sqlite::ConnectionThreadSafe) -> Result<(), 
         Ok(server) => server,
         Err(e) => return Err(e),
     };
-    for request in server.incoming_requests() {
+    for mut request in server.incoming_requests() {
         let route = (request.method(), request.url());
         println!("Handling {} {}", route.0, route.1);
         let response = match route {
             (&tiny_http::Method::Get, "/") => list_comments(&db_conn),
+            (&tiny_http::Method::Post, "/") => create_comment(&mut request, &db_conn),
             _ => Ok(default_response(404)),
         };
         match response {
